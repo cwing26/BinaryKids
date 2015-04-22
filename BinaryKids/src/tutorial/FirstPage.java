fd03ea684197db7cd5eea9f25afcfcd78f48bb45
ma797 <meghan.anand2@gmail.com>
2015-04-22 16:48:19 -0400

    changed first page

--- BinaryKids/src/tutorial/FirstPage.java
+++ BinaryKids/src/tutorial/FirstPage.java
@@ -1,4 +1,5 @@
 package tutorial;
+
 import java.awt.Button;
 import java.awt.Checkbox;
 import java.awt.CheckboxGroup;
@@ -8,11 +9,13 @@ import java.awt.RenderingHints;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JApplet;
 import javax.swing.JButton;
@@ -20,6 +23,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
@@ -52,6 +56,19 @@ public class FirstPage extends JPanel
 	TextField useField;
 	//JTextArea useField = new JTextArea(400, 400);
 	
+	//size of text area
+	private final int areaWidth = 4;
+	private final int areaHeight = 15;
+		
+	private JTextArea area = new JTextArea(areaWidth, areaHeight);
+	
+	JCheckBox skypeCheck;
+	JCheckBox gameCheck;
+	JCheckBox emailCheck;
+	JCheckBox youtubeCheck;
+	JCheckBox fbCheck;
+
+	
 	String userName = "";
 	private ImageIcon boxIcon;
 	private Image img;
@@ -61,11 +78,21 @@ public class FirstPage extends JPanel
     public FirstPage(WelcomePage welcome)
     {
     	welcomePage = welcome;
-    	setBackground(WelcomePage.backgroundColor);
+    	userName = welcomePage.getUserName();
+    	
+    	area.setFont(new Font("Monospaced",Font.PLAIN,40)); //sets font of j text area 
+		area.setBackground(Color.white);
+    	
+    	setBackground(welcomePage.backgroundColor);
+    	//scroll pane is added to middle of jFrame
+    	JScrollPane scroll = new JScrollPane(area,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+    	JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    			
+    	//setLayout(new GridLayout(0,1));
     	//userName = welcomePage.getUserName();
     	
     	JPanel titlePanel = new JPanel();
-    	JLabel titleLabel = new JLabel("Welcome to BinaryKids!");
+    	JLabel titleLabel = new JLabel("Welcome to BinaryKids, " + userName);
     	titleLabel.setFont(new Font("Verdana",1,20));
     	titlePanel.add(titleLabel);
     	titlePanel.setBorder(new LineBorder(Color.BLACK)); 
@@ -77,30 +104,69 @@ public class FirstPage extends JPanel
     	
     	submitButton2 = new JButton("Submit Answer");
     	useField = new TextField("Type your answer here");
-    	useField.setSize(400, 400);
+    	useField.setPreferredSize(new Dimension(200,24));
     	
     	//useField.setFont(new Font("Monospaced",Font.PLAIN,16)); //sets font of j text area 
 		useField.setBackground(Color.lightGray);
     	
-    	
+        //Create the check boxes.
+        skypeCheck = new JCheckBox("Skype");
+        skypeCheck.setMnemonic(KeyEvent.VK_S);
+        skypeCheck.setSelected(false);
+
+        gameCheck = new JCheckBox("Games");
+        gameCheck.setMnemonic(KeyEvent.VK_G);
+        gameCheck.setSelected(false);
+
+        emailCheck = new JCheckBox("Email");
+        emailCheck.setMnemonic(KeyEvent.VK_E);
+        emailCheck.setSelected(false);
+
+        youtubeCheck = new JCheckBox("Videos");
+        youtubeCheck.setMnemonic(KeyEvent.VK_V);
+        youtubeCheck.setSelected(false);
+        
+        fbCheck = new JCheckBox("Facebook");
+        fbCheck.setMnemonic(KeyEvent.VK_F);
+        fbCheck.setSelected(false);
+
+        //Register a listener for the check boxes.
+        skypeCheck.addItemListener(new useListener());
+        gameCheck.addItemListener(new useListener());
+        emailCheck.addItemListener(new useListener());
+        youtubeCheck.addItemListener(new useListener());
+        fbCheck.addItemListener(new useListener());
+        
+        JPanel checkPanel = new JPanel(new GridLayout(0, 1));
+        checkPanel.add(gameCheck);
+        checkPanel.add(skypeCheck);
+        checkPanel.add(emailCheck);
+        checkPanel.add(youtubeCheck);
+        checkPanel.add(fbCheck);
+
+		
+		
     	submitButton2.addActionListener(new answerButtonListener());
     	
     	add(titlePanel);
     	
     	add(questionLabel);
-    	add(useField);
+    	//add(useField);
+    	add(scroll);
+        //add(checkPanel, BorderLayout.LINE_START);
+
     	add(submitButton2);
     	
     	String obstImgFileName = "titleImage.jpg";
 		//try to load the image file
 		try {
-			boxIcon = new ImageIcon(ImageIO.read(new File(WelcomePage.obstImgFileName)));
+			boxIcon = new ImageIcon(ImageIO.read(new File(obstImgFileName)));
 		} catch (IOException e) {
 			System.out.println("Please check image file path.");
 			e.printStackTrace();
 		};
 		img = boxIcon.getImage();
-		img = img.getScaledInstance(400, 300, Image.SCALE_SMOOTH);
+		img = img.getScaledInstance(400, 250, Image.SCALE_SMOOTH);
     	
     	
     	
@@ -115,13 +181,24 @@ public class FirstPage extends JPanel
 
     }
     
+    class useListener implements ItemListener
+    {
+    	@Override
+		public void itemStateChanged(ItemEvent e) 
+    	{
+			// TODO Auto-generated method stub
+    		//if(e.getItemSelectable() == gameCheck)
+    		//else if(e.getItemSelectable() == gameCheck)
+			
+		}
 
+    }
 	
 	class answerButtonListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e) 
 		{
-			welcomePage.setUserAnswer(useField.getText());
+			welcomePage.setUserAnswer(area.getText());
 			repaint();
 		}
 	}
