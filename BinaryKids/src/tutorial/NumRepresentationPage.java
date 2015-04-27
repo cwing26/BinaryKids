9365f8f0acbc3f77fb3dcc7b1d3c5b54eacdb8b2
ma797 <meghan.anand2@gmail.com>
2015-04-27 11:45:43 -0400

    with comments and constants

--- BinaryKids/src/tutorial/NumRepresentationPage.java
+++ BinaryKids/src/tutorial/NumRepresentationPage.java
@@ -1,6 +1,6 @@
 package tutorial;
 
-
+//imports
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,15 +14,60 @@ import java.awt.Image;
 import java.awt.Toolkit;
 
 
-
+/* This class serves as the first view in the introductory learning module, 
+ * introducing the student to different systems of counting that are used
+ * with images and animation
+ */
 @SuppressWarnings("serial")
 public class NumRepresentationPage extends JPanel implements ActionListener
 {
-
-	String userName = "";
+	//text elements on screen
 	String question = "How many soccer balls are there?";
-	int soccerBallX = 30;
+	String VI = "VI";
+	String six = "6";
+	String binarySix = "110";
 	
+	//positions of text, shapes, and images on screen
+	int titleImageX = 5;
+	int titleImageY = 10;
+	
+	int questionX = 40;
+	int questionY = 150;
+	
+	int objectCount = 6;
+	
+	int soccerBallX = 50;
+	int soccerBallY = 230;
+	
+	int ovalSize = 50;
+	
+	int rectSize = 25;
+	int rectX = 350;
+	int rectY = 310;
+	
+	int decTitleImageX = 40;
+	int decTitleImageY = 100;
+	
+	int romanNumImageX = 40;
+	int romanNumImageY = 200;
+	
+	int shapesImageX = 40;
+	int shapesImageY = 300;
+	
+	int binaryImageX = 40;
+	int binaryImageY = 450;
+	
+	int sixX = 560;
+	int sixY = 140;
+	
+	int binarySixX = 535;
+	int binarySixY = 490;
+	
+	int vIX = 545;
+	int vIY = 240;
+	
+	
+	//images used on screen
 	public Image numRepTitleImage;
 	public Image decimalTitleImage;
 	public Image romanNumeralTitleImage;
@@ -31,114 +76,117 @@ public class NumRepresentationPage extends JPanel implements ActionListener
 	public Image binaryImageDark;
 	public Image soccerBall;
 	
-	int rectSize = 25;
-	int rectX = 40;
-	int rectY = 200;
+
 	
 	private WelcomePage welcomePage; //to connect welcome page to individual pages 
-			
+	
+	//handles animation on a 3 second delay
 	private Timer timer;
-	private int DELAY = 2000;
+	private int DELAY = 3000;
 	int countAnimation = 0;
 
+	//constructor sets the background color of the panel and starts the animation timer
     public NumRepresentationPage(WelcomePage welcome)
     {
     	
     	welcomePage = welcome;
-    	userName = welcomePage.getUserName();
     	setBackground(WelcomePage.backgroundColor);
-    	timer = new Timer(DELAY, this);
     	
     	loadImages();
 
-    	
     	setVisible(true);
     	
+    	//creates timer and starts animation
+    	timer = new Timer(DELAY, this);
     	timer.start();
     }
     
+    //used to draw all components of screen in proper position, 
+    //repaints based on animation count
     public void paint(Graphics g) 
     { 
     	super.paint(g);
     	
-    	g.drawImage(numRepTitleImage, 5, 10, this);
+    	g.drawImage(numRepTitleImage, titleImageX, titleImageY, this);
     	
-    	
+    	//first iteration of animation only displays question
     	if(countAnimation == 1)
     	{
     		g.setFont(new Font("Geneva", 1, 25));
-    		g.drawString(question, 40, 150);
-    		
-    		for(int i = 0; i < 6; i ++)
+    		g.drawString(question, questionX, questionY);
+
+    		for(int i = 0; i < objectCount; i ++)
     		{
-    			//g.drawImage(soccerBall, soccerBallX, 250, this);	
+    			//g.drawImage(soccerBall, soccerBallX, soccerBallY, this);	
     			g.setColor(WelcomePage.buttonPanelColor);
-    			g.fillOval(soccerBallX, 250, 50, 50);
+    			g.fillOval(soccerBallX, soccerBallY, ovalSize, ovalSize);
     			soccerBallX = soccerBallX + 100;
     		}
     		
     	}
-    	
+    	//second iteration displays answer written in several number systems 
     	else if(countAnimation == 2)
     	{
-	    	g.setColor(WelcomePage.buttonPanelColor);
-	    	g.setColor(Color.black);
+	    	g.setColor(WelcomePage.textColor);
+	    	
 	    	rectX = 350;
-	    	for(int i = 0; i < 6; i++)
+	    	for(int i = 0; i < objectCount; i++)
 	    	{
 	    		
-	    		g.fillRect(rectX, 310, rectSize, rectSize);
+	    		g.fillRect(rectX, rectY, rectSize, rectSize);
 	    		rectX = rectX + 50;
 	    		
 	    	}
 	
-	    	g.drawImage(decimalTitleImage, 40, 100, this);
-	    	g.drawImage(romanNumeralTitleImage, 40, 200, this);
-	    	g.drawImage(shapesTitleImage, 40, 300, this);
-    		g.drawImage(binaryImageLight, 40, 450, this);
+	    	g.drawImage(decimalTitleImage, decTitleImageX, decTitleImageY, this);
+	    	g.drawImage(romanNumeralTitleImage, romanNumImageX, romanNumImageY, this);
+	    	g.drawImage(shapesTitleImage, shapesImageX, shapesImageY, this);
+    		g.drawImage(binaryImageLight, binaryImageX, binaryImageY, this);
 
 	    	g.setColor(WelcomePage.textColor);
 	    	g.setFont(new Font("Monospaced", 1, 60));
-	    	g.drawString("6", 560, 140);
-	    	g.drawString("VI", 545, 240);
-	    	g.drawString("110", 535, 490);
+	    	g.drawString(six, sixX, sixY);
+	    	g.drawString(VI, vIX, vIY);
+	    	g.drawString(binarySix, binarySixX, binarySixY);
     	
     	
     	}
-  	    else if(countAnimation > 2)
+    	//third iteration highlights binary number answer
+  	    else if(countAnimation >= 3)
   	    {
-  	    	g.drawImage(binaryImageDark, 40, 450, this);
+  	    	g.drawImage(binaryImageDark, binaryImageX, binaryImageY, this);
   	    	g.setColor(WelcomePage.textColor);
   	    	g.drawRect(30, 435, 650, 80);
   	    	g.setFont(new Font("Monospaced", 1, 60));
   	    	g.drawString("=", 560, 415);
   	    	
-	    	g.setColor(WelcomePage.buttonPanelColor);
-	    	g.setColor(Color.black);
+  	    	g.setColor(WelcomePage.textColor);
+	    	
 	    	rectX = 350;
-	    	for(int i = 0; i < 6; i++)
+	    	for(int i = 0; i < objectCount; i++)
 	    	{
 	    		
-	    		g.fillRect(rectX, 310, rectSize, rectSize);
+	    		g.fillRect(rectX, rectY, rectSize, rectSize);
 	    		rectX = rectX + 50;
 	    		
 	    	}
-
-	    	g.drawImage(decimalTitleImage, 40, 100, this);
-	    	g.drawImage(romanNumeralTitleImage, 40, 200, this);
-	    	g.drawImage(shapesTitleImage, 40, 300, this);
+	
+	    	g.drawImage(decimalTitleImage, decTitleImageX, decTitleImageY, this);
+	    	g.drawImage(romanNumeralTitleImage, romanNumImageX, romanNumImageY, this);
+	    	g.drawImage(shapesTitleImage, shapesImageX, shapesImageY, this);
 
 	    	g.setColor(WelcomePage.textColor);
 	    	g.setFont(new Font("Monospaced", 1, 60));
-	    	g.drawString("6", 560, 140);
-	    	g.drawString("VI", 545, 240);
-	    	g.drawString("110", 535, 490);
+	    	g.drawString(six, sixX, sixY);
+	    	g.drawString(VI, vIX, vIY);
+	    	g.drawString(binarySix, binarySixX, binarySixY);
 	    		
   	    }
 	
     }
     
     
+    //WILL BE TRANSFERRED TO welcomepage
     public void loadImages()
     {
 		
@@ -173,6 +221,11 @@ public class NumRepresentationPage extends JPanel implements ActionListener
 		
     }
     
+    
+    //class implements actions listener to handle animation
+    //and thus must override action performed
+    //method just increments an animation counter
+    //to keep track of what stage the display is in
 	@Override
     public void actionPerformed(ActionEvent e) 
 	{
